f191727718bb379f0ef233000fd2e7cc4e7c69a9
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-12-15 20:43:57 -0600

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+const livepeerClient = createReactClient( {
+  provider: studioProvider( {
+    apiKey: process.env.LIVEPEER_API
+  })
+})
+
+ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <LivepeerConfig client={livepeerClient}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </LivepeerConfig>
+    </>
+  );
 }
